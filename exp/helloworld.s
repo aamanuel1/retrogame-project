b50535f58a62d49f39b64ea11cfdbf4deaf1d527
Aaron Manuel <aaron.manuel@ucalgary.ca>
2024-10-02 23:27:47 -0600

    Add comments to helloworld for future

--- exp/helloworld.s
+++ exp/helloworld.s
@@ -8,41 +8,41 @@ CHROUT = $ffd2
 	org $1001
 
 ;BASIC stub
-	dc.w nextstmt
-	dc.w 10
-	dc.b $9e, "4109", 0
+	dc.w nextstmt		
+	dc.w 10			;Line number
+	dc.b $9e, "4109", 0	;sys 4109, end of statement
 nextstmt
-	dc.w 0
+	dc.w 0			;End of basic stub, start of assembly
 
 ;PROGRAM START	
-	lda #>helloworld
-	sta $21
-	lda #<helloworld
-	sta $20
-	jsr printstring
-	rts
+	lda #>helloworld	;Load high byte
+	sta $21			;Store in 20, 21 (lower zero page addr)
+	lda #<helloworld	;Load low byte
+	sta $20			;store in zero page 20
+	jsr printstring		;Jump but return from printstring
+	rts			;return from printstring and continue
 
 printstring
-	ldy #0
+	ldy #0			;Load 0 to start increment
 printstring_loop
-	lda ($20),y
-	cmp #255
-	beq printstring_end
-	jsr printchar
-	iny
-	jmp printstring_loop
+	lda ($20),y		;Indirect indexed address, zero page $20 + y
+	cmp #255		;Compare value of A with 255 terminator
+	beq printstring_end	;Return to calling subroutine
+	jsr printchar		;Otherwise print the character at A
+	iny			;Increment Y to move down the string
+	jmp printstring_loop	;loop through until the end
 
 printstring_end
-	rts
+	rts			;The end
 
 printchar
-	cmp #96
-	bcc printchar_ok
-	and #%11011111
+	cmp #96			;Check if the letter is a lowercase
+	bcc printchar_ok	;Carry clear means that A <= 96, so it's uppercase already
+	and #%11011111		;Flip bit 5 to offset down by 32 (-32)
 printchar_ok
-	jsr CHROUT
-	rts
+	jsr CHROUT		;Print the char
+	rts			;return to calling subroute
 
 helloworld
-	dc.b "Hello world",255
+	dc.b "Hello world",255	;Data section, THE string.
 
