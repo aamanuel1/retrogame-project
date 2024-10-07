bf744fc4a406499c93bd0eef86521843c8e36e3c
Aaron Manuel <aaron.manuel@ucalgary.ca>
2024-10-06 18:17:55 -0600

    Got the loop to work for loading settings

---
+++ exp/basicmov.s
@@ -0,0 +1,55 @@
+;basicmov.s Shows basic movement on a blank board.
+
+	processor 6502
+
+;KERNAL ADDRESSES
+
+CHROUT = $ffd2
+
+;ORIGIN
+	org $1001
+
+;BASIC STUB
+	dc.w nextstmt
+	dc.w 10				;line number
+	dc.b $9e, "4109", 0		;SYS 4109, end of statement
+nextstmt
+	dc.w 0				;end of basic stub
+
+;PROGRAM START
+
+	
+init:					;Initialize video settings on VIC chip
+	ldy #16				;Iterator
+init_loop:
+	dey
+	lda videosettings,Y
+	sta $9000,Y
+	tya
+	bne init_loop
+	
+	lda #'A
+	jsr CHROUT
+	lda #'S
+	jsr CHROUT
+	jsr CHROUT 
+	rts  
+
+videosettings:
+	dc.b %00001100			;9000 $05 Interlace off, screen origin horiz 12  (def 5, lower value left
+	dc.b %00011001			;9001 $19 Screen origin vertical 5, lower value up by 2pix to 1
+	dc.b %10010110			;9002 $96 bit 7 Screen mem location, rest columns (default 22 max 27)
+	dc.b %10101110			;9003 $AE bit 7 is screen raster, bit 6-1  rows (default 23 max 23)
+					;bit 0 is character size 0 8x8 chars, 1 8x16 chars
+	dc.b %01111010			;9004 $7A 122 TV raster beam line
+	dc.b %11110001			;9005 $F0 bits 4-7 video address, bit 0-3 character memory loc.
+	dc.b %0				;9006 $0 Horizontal light pen location
+	dc.b %0				;9007 $0 Vertical light pen location
+	dc.b %11111111			;9008 $FF Paddle X Digitized variable resistance
+	dc.b %11111111			;9009 $FF Paddle Y Digitized variable resistance
+	dc.b %0				;900A $0 Bass oscillator
+	dc.b %0				;900B $0 Alto oscillator
+	dc.b %0				;900C $0 Soprano oscillator
+	dc.b %0				;900D $0 Noise source
+	dc.b %0				;900E $0 Bit 4-7 auxilary colour, bit 0-3 sound loudness
+	dc.b %00011011			;900F 
