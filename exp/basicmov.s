9e192d27cd7c5f44b96db1c289c79704cdf595ed
Aaron Manuel <aaron.manuel@ucalgary.ca>
2024-10-07 18:17:15 -0600

    Cleared the screen and played with screen codes

--- exp/basicmov.s
+++ exp/basicmov.s
@@ -27,12 +27,27 @@ init_loop:
 	sta $9000,Y
 	tya
 	bne init_loop
-	
-	lda #'A
-	jsr CHROUT
-	lda #'S
-	jsr CHROUT
-	jsr CHROUT 
+
+clearscreen:
+	lda #$0
+	sta $01
+	lda #$1e
+	sta $02
+	ldy #$ff
+clearscreen_loop:
+	lda #$20
+	sta ($01),Y
+	dey
+	bne clearscreen_loop
+
+main:	
+	lda #1
+	sta ($01),Y
+	iny
+	lda #19
+	sta ($01),Y
+	iny
+	sta ($01),Y
 	rts  
 
 videosettings:
@@ -42,7 +57,7 @@ videosettings:
 	dc.b %10101110			;9003 $AE bit 7 is screen raster, bit 6-1  rows (default 23 max 23)
 					;bit 0 is character size 0 8x8 chars, 1 8x16 chars
 	dc.b %01111010			;9004 $7A 122 TV raster beam line
-	dc.b %11110001			;9005 $F0 bits 4-7 video address, bit 0-3 character memory loc.
+	dc.b %11110000			;9005 $F0 bits 4-7 video address, bit 0-3 character memory loc.
 	dc.b %0				;9006 $0 Horizontal light pen location
 	dc.b %0				;9007 $0 Vertical light pen location
 	dc.b %11111111			;9008 $FF Paddle X Digitized variable resistance
