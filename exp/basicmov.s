6628112302e641d1f9edc68e94b8b333457af06d
Aaron Manuel <aaron.manuel@ucalgary.ca>
2024-10-11 11:40:48 -0600

    Add comments

--- exp/basicmov.s
+++ exp/basicmov.s
@@ -61,17 +61,17 @@ main:
 	ldx #0
 	lda #<PLRSTRT
 	sta $01
-	lda #>PLRSTRT
+	lda #>PLRSTRT			;Player starts at 7910, $1ee6
 	sta $02
-	lda #10
-	sta PLRX
-	lda #10
-	sta PLRY
-	lda #<PLRCOLR
+	lda #10				
+	sta PLRX			;Offset x by 10, $0a TODO: better way to do this?
+	lda #10				;Range 0-22
+	sta PLRY			;Offset y by 10
+	lda #<PLRCOLR			;Player colour map at 38638, $96e6
 	sta $05
 	lda #>PLRCOLR
 	sta $06
-	lda #2
+	lda #2				;Load character and colour into low bytes of maps
 	sta ($05,X)
 	lda #81
 	sta ($01,X)
@@ -105,41 +105,41 @@ end_poll:
 	jmp game_loop			;End of "game" loop
 	
 move_left:
-	lda #32
+	lda #32				;Clear current location with SPACE
 	sta ($01,X)
-	lda $01
+	lda $01				;Load current location for operations
+	sec				;DON'T forget to set this.
+	sbc #1				;subtract 1 to move left
+	sta $01				;Store in low byte of screen map
+	lda $05				;Load current colour map location
 	sec
-	sbc #1
-	sta $01
-	lda $05
-	sec
-	sbc #1
-	sta $05
-	bcs check_left
-	lda $02
-	cmp #$1E
-	beq check_left
-	dec $02
-	dec $06
+	sbc #1				;Subtract by 1 to colour left tile
+	sta $05				;Store in low byte of screen map
+	bcs check_left			;Do a check of the location if didn't roll over
+	lda $02				;If we did roll over (carry clear)
+	cmp #$1E			;Do a check if we're in $1F
+	beq check_left			;If we're not then continue the ckeck
+	dec $02				;Otherwise, turn $1F to $1E
+	dec $06				;turn $97 to $96
 check_left:
-	dec PLRX
-	jsr global_collision
-	beq draw_left
-	inc $01
+	dec PLRX			;decrement player X position
+	jsr global_collision		;Check if it's bumping against edge of map
+	beq draw_left			;If it's 0 then keep going
+	inc $01				;If it's 1 then reset everything.
 	inc $05
-	inc PLRX
-	jmp draw_left
+	inc PLRX			;Reset the X counter
+	jmp draw_left			;Draw it not moving.
 draw_left:
-	lda #81
+	lda #81				;Draw a circle...
 	sta ($01,X)
-	lda #2
+	lda #2				;That is red.
 	sta ($05,X)
 end_move_left:
-	jsr delay
+	jsr delay			;Runs fast, need to figure that out.
 	jmp game_loop
 
 move_right:
-	lda #32
+	lda #32				;Same as above but we're adding 1 to move right
 	sta ($01,X)
 	lda $01
 	clc
@@ -173,27 +173,27 @@ end_move_right:
 	jmp game_loop
 
 move_up:
-	lda #32
+	lda #32				;Same as above, but we're subtracting by 22 to move up
 	sta ($01,X)
-	lda $01
-	sec
-	sbc #22
-	sta $01
-	lda $05
-	sec
+	lda $01				;Load low byte of current location
+	sec				;Set carry flag, carry clear means we borrowed
+	sbc #22				;Subtract by 22 because the space above is offset by 22 (22 cols)
+	sta $01				;Store new location in screen location
+	lda $05				;Do the same with the colour map
+	sec		
 	sbc #22
 	sta $05
-	bcs check_up
+	bcs check_up			;Carry set means we didn't roll over
 	lda $02
-	cmp #$1e
-	beq check_up
-	dec $02
-	dec $06
+	cmp #$1e			;The carry was clear (we rolled over) so check if it's already in 1e
+	beq check_up			;If it's in $1exx then keep going (maybe we don't need this)
+	dec $02				;Otherwise it's in $1f so bring it to $1e
+	dec $06				;bring from $97 to $96
 check_up:
-	dec PLRY
-	jsr global_collision
-	beq draw_up
-	lda $01
+	dec PLRY			;Decrement by 1
+	jsr global_collision		;Check if it's collided with the edge
+	beq draw_up			;If false, continue drawing
+	lda $01				;If true then reset everything by adding it back or incrementing it back
 	clc
 	adc #22
 	sta $01
@@ -202,19 +202,19 @@ check_up:
 	adc #22
 	sta $05
 	inc PLRY
-	jmp draw_up
+	jmp draw_up			;Draw it not moving
 draw_up:
-	lda #81
+	lda #81				;Draw a red circle
 	sta ($01,X)
 	lda #2
 	sta ($05,X)
 end_move_up:
-	jsr delay
+	jsr delay			;Delay to make it less fast
 	jsr delay
 	jmp game_loop
 
-move_down:
-	lda #32
+move_down:				;Same as above but add 22 to move down
+	lda #32					
 	sta ($01,X)
 	lda $01
 	clc
@@ -254,24 +254,24 @@ end_move_down:
 	jmp game_loop
 
 shoot:
-	lda BRDR_SCR_COLOUR
+	lda BRDR_SCR_COLOUR		;Change the screen colour to yellow
 	and #%11111111
 	ora #%11110000
 	sta BRDR_SCR_COLOUR
 	jmp main
 
 global_collision:
-	lda PLRX
+	lda PLRX			;Load player X position
 	cmp #0
-	bmi collide
+	bmi collide			;X < 0 we collided with left edge
 	cmp #22
-	bpl collide
-	lda PLRY
-	cmp #0
-	bmi collide
+	bpl collide			;X > 22 we collided with right edge
+	lda PLRY			;Load player Y position
+	cmp #0	
+	bmi collide			;X < 0 we collided with top edge
 	cmp #23
-	bpl collide
-;	lda $02
+	bpl collide			;X > 23 we collided with bottom edge
+;	lda $02				;Some old stuff might delete later.
 ;	sec
 ;	sbc #$1e
 ;	bcc collide
@@ -281,14 +281,13 @@ global_collision:
 ;	lda $01
 ;	cmp #$f9
 ;	bpl collide
-	lda #0
-
+	lda #0				;Return false
 	jmp ret_global_collision
 collide:
-	lda #1
+	lda #1				;Return true
 	jmp ret_global_collision
 ret_global_collision:
-	rts
+	rts				;Return result.
 
 delay:
 	ldx #$ff			;Delay 255 times
