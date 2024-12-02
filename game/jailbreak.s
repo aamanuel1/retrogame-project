492ab1e8a7b6237401962a906a31ccd497956bca
Aaron Manuel <aaron.manuel@ucalgary.ca>
2024-12-01 18:47:44 -0700

    Removed colour markers in zero page for now, don't really need them.

--- game/jailbreak.s
+++ game/jailbreak.s
@@ -48,12 +48,12 @@ PLAYER_LIVES = $08
 PLAYER_ALIVE = $09
 PLAYER_ADDR_LO = $0A
 PLAYER_ADDR_HI = $0B
-PLAYER_COLOUR_LO = $0C
-PLAYER_COLOUR_HI = $0D
+; PLAYER_COLOUR_LO = $0C
+; PLAYER_COLOUR_HI = $0D
 
 ;OBJECTS
 	SEG.U enemies
-	ORG $0E
+	ORG $0C
 num_enemies ds 1
 enemy_sprite ds 8
 enemy_dir ds 8
@@ -64,7 +64,7 @@ enemy_high ds 8
 enemy_alive ds 8
 
 	SEG.U bullets
-	ORG $47
+	ORG $45
 numbullet ds 1
 bullet_sprite ds 8
 bullet_dir ds 8
@@ -72,26 +72,26 @@ bullet_x ds 8
 bullet_y ds 8
 bullet_low ds 8
 bullet_high ds 8
-bullet_colour ds 8			;To delete if needed
+; bullet_colour ds 8			;To delete if needed
 bullet_collide ds 8
 	SEG
 
-CUR_LEVEL = $80
-CUR_SPRITE = $81
-OBJECT_DIR = $82
-COUNTER = $83
-COLLISION_STATUS = $84
-SCRATCH_LO = $85
-SCRATCH_HI = $86
-REMAINDER = $87
-ENEMY_SHOOT_TIMER = $88
-ENEMY_MOVE_TIMER = $89
-ENEMY_CYCLE_CTR = $8A
-ENEMY_DIFF_X = $8B
-ENEMY_DIFF_Y = $8C
-ENEMY_COUNTER = $8D
-COLL_PTR_LO = $8E
-COLL_PTR_HI = $8F
+CUR_LEVEL = $76
+CUR_SPRITE = $77
+OBJECT_DIR = $78
+COUNTER = $79
+COLLISION_STATUS = $7A
+SCRATCH_LO = $7B
+SCRATCH_HI = $7C
+REMAINDER = $7D
+ENEMY_SHOOT_TIMER = $7E
+ENEMY_MOVE_TIMER = $7F
+ENEMY_CYCLE_CTR = $80
+ENEMY_DIFF_X = $81
+ENEMY_DIFF_Y = $82
+ENEMY_COUNTER = $83
+COLL_PTR_LO = $84
+COLL_PTR_HI = $85
 
 ;ORIGIN
 	org $1001
