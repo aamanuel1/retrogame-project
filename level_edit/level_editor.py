4eb08abf2bd095b3e1ba7f0fc18fe3e73be028b8
Aaron Manuel <aaron.manuel@ucalgary.ca>
2025-02-03 19:33:54 -0700

    Fixed level editor to better chunk the bytestring, levels.txt had missing row

--- level_edit/level_editor.py
+++ level_edit/level_editor.py
@@ -17,9 +17,9 @@ def compress_all_levels(level_data, level_layout):
         level_string += level_name + ':\n'
         
         #[N, E, S, W]
-        level_adj_list = [level_index+3, 
+        level_adj_list = [level_index-3, 
                           level_index+1,
-                          level_index-3,
+                          level_index+3,
                           level_index-1]
 
         for level_neighbours in level_adj_list:
@@ -29,9 +29,9 @@ def compress_all_levels(level_data, level_layout):
                 neighbour_loc = f'\tdc.b\t<{level_layout[level_neighbours]}, >{level_layout[level_neighbours]}\n'
             level_string += neighbour_loc
 
-        #506 blocks + 22 \r\n or \n's == 528
+        #506 blocks + 23 \r\n or \n's == 528
         print(level_name)
-        compressed_level = compress_level(level_data.read(506)) + '\n'
+        compressed_level = compress_level(level_data.read(529)) + '\n'
         level_string += compressed_level
 
     return level_string
@@ -41,27 +41,35 @@ def compress_level(data):
     compressed_level = []
     level_length = 506
     i = 0
+    cleaned_data = data.replace('\n', '')
 
     while i < level_length:
         run_length = 1
 
-        while (i + run_length < len(data) and
-            data[i] == data[i + run_length] and
+        while (i + run_length < len(cleaned_data) and
+            (cleaned_data[i] == cleaned_data[i + run_length] or
+             cleaned_data[i + run_length] == '\n')and
             run_length < 255):
+            # if cleaned_data[i + run_length] == '\n':
+            #     continue
+            # print("something")
             run_length += 1
-            # print(data[total_length], end='')
-            # if not (total_length % 23):
-            #     print('\n', end='')
 
         run_length_string = '${:02X}'.format(run_length)
         compressed_level.append(run_length_string)
 
         #I bet this will bite me in the ass later.
-        if(i < len(data)):  #Need this check because of some final level access issue.
-            compressed_level.append(level_objs.get(data[i], '$20'))
+        #It did.
+        if(i < len(cleaned_data)):  #Need this check because of some final level access issue.
+            compressed_level.append(level_objs.get(cleaned_data[i], '$20'))
         i += run_length
 
     print(i)
+    i = 0
+    while(i < len(data)):
+        if not (i % 21):
+            compressed_level.insert(i, "\n\tdc.b\t")
+        i += 1
     finished_compressed_level = ",".join(compressed_level)
     finished_compressed_level = "\tdc.b\t" + finished_compressed_level
     return finished_compressed_level
